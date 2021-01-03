940e64194ba852e282cb570a0087da624f8ed6f5
jhonathanmusa <jhonathansamu91@gmail.com>
2021-01-02 22:58:02 -0500

    adding some components 1

---
+++ src/components/Form.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    console.log(this.state.value);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Say Something..."
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        {/* <p>{this.state.value}</p> */}
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default Input;
